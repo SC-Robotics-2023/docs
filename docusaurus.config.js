411d509a6761163f41dbb7e72b21f20c8bcd9e8a
Jacob <79492016+fngld@users.noreply.github.com>
2023-03-07 18:20:25 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   title: "SC Robotics",
   tagline: "Rovers are cool",
   url: `https://${organizationName}.github.io`,
-  baseUrl: `/${projectName}/`,
+  baseUrl: `/docs/`,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   favicon: "img/favicon.ico",
