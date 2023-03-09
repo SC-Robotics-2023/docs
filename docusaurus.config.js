edee7e37684e9bcc6e4b1e75d221f2d455cb6e03
Jacob <79492016+fngld@users.noreply.github.com>
2023-03-09 14:39:33 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://sc-robotics-2023.github.io/knowledge_base/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/knowledge_base/',
